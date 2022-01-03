ce868056e568071ecc66c7351a1debbf09206eae
km6293 <alstn7616@naver.com>
2022-01-03 23:48:02 +0900

    씹창코드 완성

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -12,7 +12,8 @@
         <div class="chat-widget">
           <div class="chat-section">
             <span v-for="a in data" :key="a.chat">
-              <span class="short-date">{{timestamp_to_date(a)}}</span>
+              <span class="short-date" v-if="isNaN(a.timestamp) == true">{{a.timestamp}}</span>
+              <span class="short-date" v-if="isNaN(a.timestamp) == false">{{timestamp_to_date(a.timestamp)}}</span>
               <a class="user">{{a.name}}: </a>
               <span class="message">{{a.chat}}</span>
               <br/>
@@ -28,6 +29,39 @@
 </template>
 <script>
 export default {
+  // 무한루프 걸립니다 선생님
+  // watch: {
+  //   data(a){
+  //     this.sort_date()
+  //   },
+  //   immediate: true // 컴포넌트가 생성되자마자 즉시 실행
+  // },
+
+  methods: {
+    timestamp_to_date(news){
+      var date = new Date(news * 1000 - 60 * 60 * 9 * 1000)
+      var hour = this.correction_date(date.getHours())
+      var min = this.correction_date(date.getMinutes())
+      return `${hour}:${min}`
+    },
+    correction_date(i){
+      return i >= 10 ? i : '0' + i
+    },
+    sort_date(){
+      this.data.sort( (a,b) => parseFloat(b.timestamp) - parseFloat(a.timestamp ) )
+    },
+    push_data(){
+      var inputDate = (Date.now() /1000 | 0)
+      var pushData = {
+        'name' : 'test',
+        'chat' : this.inputText,
+        'timestamp' : this.timestamp_to_date(inputDate)
+      }
+      this.data.push(pushData);
+
+    }
+  },
+
   name: 'HelloWorld',
   data () {
     return {
@@ -50,32 +84,6 @@ export default {
           },
       ]
     }
-  },
-  methods: {
-    timestamp_to_date(news){
-      var date = new Date(news.timestamp * 1000 - 60 * 60 * 9 * 1000)
-      var hour = this.correction_date(date.getHours())
-      var min = this.correction_date(date.getMinutes())
-      return `${hour}:${min}`
-    },
-    correction_date(i){
-      return i >= 10 ? i : '0' + i
-    },
-    sort_date(){
-      this.data.sort( (a,b) => parseFloat(a.timestamp) - parseFloat(b.timestamp ) )
-    },
-    // 이곳 포맷 하고 넣어야되는지 .... 56줄 계산해보니까 now 자릿수랑 똑같아서 의문임 그리고 되지도 않음ㅅㅂㅅㅂ
-    // push_data(){
-    //   // alert(this.inputText);
-    //   var inputDate = Date.now();
-    //   var hour = this.correction_date(inputDate.getHours())
-    //   var min = this.correction_date(inputDate.getMinutes())
-    //   alert(`${hour}:${min}`)
-    // }
-  },
-  // error - 라이프사이클 훅 의문.. 처음 랜더링 될때 데이터 정리하고 보여주려고 하는데 안됨 ㅠ
-  beforeMount(){
-    sort_date();
   },
 
 }
